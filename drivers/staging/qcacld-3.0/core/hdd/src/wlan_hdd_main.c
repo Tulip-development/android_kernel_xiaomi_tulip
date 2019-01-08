5ec548a177b2e2dfc335142227cbd7eb80de7e69
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-01-08 21:18:30 +0700

    Revert "qcacld-3.0: guard unused function for in-kernel builds"
    This reverts commit ae568ab9898a46b8732f1a0583bffb8a60f03f60.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -11234,7 +11234,6 @@ dev_alloc_err:
 	return -ENODEV;
 }
 
-#ifdef MODULE
 static void wlan_hdd_state_ctrl_param_destroy(void)
 {
 	cdev_del(&wlan_hdd_state_cdev);
@@ -11244,7 +11243,6 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 
 	pr_info("Device node unregistered");
 }
-#endif
 
 #ifndef MODULE
 static int hdd_wait_for_probe_complete(void)
